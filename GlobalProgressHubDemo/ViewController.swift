e0e4b51478f1c13e3fc6e201bcd21ca043923b27
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2017-01-12 20:35:59 +0700

    - Install `PKHUB`. - Display progress with subtitle.

--- GlobalProgressHubDemo/ViewController.swift
+++ GlobalProgressHubDemo/ViewController.swift
@@ -7,19 +7,15 @@
 //
 
 import UIKit
+import PKHUD
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        HUD.show(.labeledProgress(title: "", subtitle: "Loading..."))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'GlobalProgressHubDemo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'PKHUD', :git => 'https://github.com/toyship/PKHUD.git'
+
+  # Pods for GlobalProgressHubDemo
+
+end
